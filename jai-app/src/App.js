633092ceb8ea824b7b6d25dc65887bbadf4226cf
Jai Uparkar <91229562+jai-uparkar@users.noreply.github.com>
2023-01-19 18:07:16 -0800

    Update App.js

--- jai-app/src/App.js
+++ jai-app/src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Hello world, I am Jai Uparkar. 
         </p>
         <a
           className="App-link"
